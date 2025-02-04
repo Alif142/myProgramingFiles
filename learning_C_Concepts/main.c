a76d65d2de38c7c6c568463c1db766e696cff395
Alif142 <mizan.alif190@gmail.com>
2025-02-04 18:42:30 +0600

    Added c filse

--- learning_C_Concepts/main.c
+++ learning_C_Concepts/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void) {
+
   int **array;
   int row, col;
   printf("Enter row and column:");
   scanf("%d%d", &row, &col);
+
   array = (int **)malloc(sizeof(int *) * row);
   for (int i = 0; i < row; i++) {
     array[i] = (int *)malloc(sizeof(int) * col);
@@ -16,6 +19,7 @@ int main(void) {
       scanf("%d", &array[i][j]);
     }
   }
+
   printf("Getting output:\n");
   for (int i = 0; i < row; i++) {
     for (int j = 0; j < col; j++) {
@@ -23,4 +27,5 @@ int main(void) {
     }
     printf("\n");
   }
+
 }
