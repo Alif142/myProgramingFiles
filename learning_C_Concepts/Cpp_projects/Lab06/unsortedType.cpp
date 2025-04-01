3a2dd8b6fa1bf422df4ab7a24526db31c1953338
Alif142 <mizan.alif190@gmail.com>
2025-04-01 21:57:58 +0600

    Updated Assignment-c

--- learning_C_Concepts/Cpp_projects/Lab06/unsortedType.cpp
+++ learning_C_Concepts/Cpp_projects/Lab06/unsortedType.cpp
@@ -1,11 +1,96 @@
+#include <cstddef>
+#include <iostream>
 #include "unsortedType.h"
+using namespace std;
 
-unsortedType::unsortedType()
-{
-    //ctor
+template <class T>
+unsortedType<T>::unsortedType(){
+    head = NULL;
+    pointTo = NULL;
+    size = 0;
+}
+template <class T>
+unsortedType<T>::~unsortedType(){
+    MakeEmpty();
+}
+template <class T>
+int unsortedType<T>::Length(){
+    return size;
+}
+template <class T>
+void unsortedType<T>::Insert(T value){
+    Node *temp = new Node;
+    temp->data = value;
+    temp->next = head;
+    head = temp;
+    size++;
+
+}
+template <class T>
+void unsortedType<T>::Search(T value , bool &found){
+    Node *i = head;
+    found = false;
+    while (i != NULL) {
+        if (i->data == value) {
+            found = true;
+            break;
+        } 
+        else
+            i = i->next;
+    }
 }
 
-unsortedType::~unsortedType()
-{
-    //dtor
+template <class T>
+void unsortedType<T>::Delete(T value){
+    Node *i = head;
+    Node *prev = NULL;
+    bool found = false;
+
+    while (i != NULL) {
+        if (i->data == value) {
+            found = true; 
+            break;
+        } 
+        else
+        i = i->next;
+    }
+    if (found) {
+        if (prev == NULL) {
+            head = i->next; 
+        }      
+        else {
+            prev->next = i->next;
+        }
+        delete i;
+        size--;
+    }
 }
+
+template <class T>
+void unsortedType<T>::MakeEmpty(){
+    Node *i = head;
+    Node *next;
+    while (i != NULL) {
+        next = i->next; 
+        delete i;
+        i = next;
+    }
+    head = NULL;
+    size = 0;
+}
+template <class T>
+void unsortedType<T>::GetNext(T &value){
+    if (pointTo == NULL) {
+        pointTo = head; 
+        value = pointTo->data;
+    } 
+    else {
+        value = pointTo->data;
+    }
+    pointTo = pointTo->next;
+}
+template <class T>
+void unsortedType<T>::Reset(){
+    pointTo = NULL;
+}
+
--- learning_C_Concepts/main.cpp
+++ learning_C_Concepts/main.cpp
@@ -1,9 +1,23 @@
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+void merge(vector<int>& nums1 , int m ,vector<int>& nums2 , int n){
+    for (int i=0;i<nums1.size();i++) {
+        nums1.erase(find(nums1.begin() , nums1.end(),0)); 
+    }
+    nums1.insert(nums1.end() ,nums2.begin(), nums2.end());
+    sort(nums1.begin(),nums1.end());
+}
+int removeElement(vector<int>& nums, int val) {
+    erase();
+    return k; 
+} 
 int main(){
-    int b = 42;
-    int *a = &b;
-    printf("b:%d\n",b);
-    printf("&b:%p\n",&b);
-    printf("a:%p\n",a);
-    return 0;
+    vector<int> nums = {3,2,2,3};
+    int val = 2;
+    removeElement(nums,val);
+    for (int i=0;i<nums.size();i++) {
+        cout<<nums[i]<<" "; 
+    }
 }
